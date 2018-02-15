5f5fd064a4078ae7b270dfae8516948933cc66ac
zbruno <zachcbruno@gmail.com>
2018-02-14 18:20:50 -0800

    yolo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Schema;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
             // Return the html tag for the asset
             return '<?php echo \'<script src="'.$assetPath.'"></script>\' ?>';
         });
+
+        Schema::defaultStringLength(191);
     }
 
     /**
